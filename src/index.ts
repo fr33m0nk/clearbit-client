2ac1da117811a0fcdccf5a9f2a20df9c5287a979
fr33m0nk <prashantsinha@protonmail.com>
2023-07-24 23:53:22 +0530

    fr33m0nk: Exports client functions and types via index.ts

---
+++ src/index.ts
@@ -0,0 +1,11 @@
+export { PersonAndCompany } from './schema/combined'
+export { Company } from './schema/company'
+export { Person } from './schema/person'
+export { ParseError } from '@effect/schema/ParseResult'
+export { FetchError, ResponseError, JSONError, APIError } from './safe_fetch'
+export {
+    combinedQuery, CombinedQueryParams,
+    CombinedQuery, personQuery,
+    PersonQueryParams, PersonQuery,
+    companyQuery, CompanyQueryParams, CompanyQuery
+  } from './client'
